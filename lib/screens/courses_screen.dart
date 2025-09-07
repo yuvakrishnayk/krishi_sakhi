0b0f87a8d833e03c7b61df00940e58db50a7bade
Jaga0001 <144882407+Jaga0001@users.noreply.github.com>
2025-09-07 15:03:08 +0530

    Optimised

--- lib/screens/courses_screen.dart
+++ lib/screens/courses_screen.dart
@@ -1,35 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:krishi_sakhi/components/drawer.dart';
+import 'package:krishi_sakhi/models/courses_model.dart';
 
-class Course {
-  final String title;
-  final String instructor;
-  final String duration;
-  final String language;
-  final String imageUrl;
-  final double rating;
-  final int students;
-  final String category;
-  final String level;
-  final bool isFree;
-  final double? price;
-  final String description;
-
-  Course({
-    required this.title,
-    required this.instructor,
-    required this.duration,
-    required this.language,
-    required this.imageUrl,
-    required this.rating,
-    required this.students,
-    required this.category,
-    required this.level,
-    required this.isFree,
-    this.price,
-    required this.description,
-  });
-}
 
 class CoursesScreen extends StatefulWidget {
   CoursesScreen({Key? key}) : super(key: key);
